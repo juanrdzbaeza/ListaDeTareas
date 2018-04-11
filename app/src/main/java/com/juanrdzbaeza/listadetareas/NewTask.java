7459376d94145d08c79941abdb65d6313420fef9
Juan Rodríguez Baeza <juanrdzbaeza@gmail.com>
2018-04-11 09:29:54 +0200

    se crea vista aparte para el menu de la pantalla NewTask. elimino boton prueva para volver a la actividad principal, esta accion la recoge el boton cancelar del menu.

--- app/src/main/java/com/juanrdzbaeza/listadetareas/NewTask.java
+++ app/src/main/java/com/juanrdzbaeza/listadetareas/NewTask.java
@@ -1,10 +1,13 @@
 package com.juanrdzbaeza.listadetareas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class NewTask extends AppCompatActivity {
 
@@ -25,12 +28,25 @@ public class NewTask extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu_new_task, menu);
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){ // TODO: 10/4/18 controlar las acciones de los distintos botones del menu.
+            case R.id.action_save:
+                Toast.makeText(this, "ha seleccionado el boton para salvar la tarea", Toast.LENGTH_SHORT).show();
 
-    public void paTras(View v) {
-        finish();
+                return true;
+            case R.id.action_cancel:
+                Toast.makeText(this, "ha seleccionado el boton para cancelar la tarea", Toast.LENGTH_SHORT).show();
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
+
+
 }
